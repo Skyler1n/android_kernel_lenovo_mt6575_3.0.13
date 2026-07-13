f95fe985808b57412bf1d98087497fea8102717f
Skyler1n <Skyler1n@outlook.com>
2026-07-13 23:16:54 +0800

    fix(dumchar): match A690 NAND partition layout
    Replace the W732 EMMC partition table in the dum-char driver with
    the A690 NAND layout.  The previous table referenced emmc-only
    partitions (mbr/ebr1/ebr2/pmt) and caused a BUG_ON(NULL) crash
    when nvram_daemon opened /proc/dumchar_info because misc was
    not found in the PartInfo array.
    
    Board: lcsh75_gb2 (Lenovo A690)
    Signed-off-by: Skyler1n <Skyler1n@outlook.com>

--- mediatek/platform/mt6575/kernel/drivers/dum-char/partition_define.h
+++ mediatek/platform/mt6575/kernel/drivers/dum-char/partition_define.h
@@ -1,41 +1,36 @@
-
 #ifndef __PARTITION_DEFINE_H__
 #define __PARTITION_DEFINE_H__
 
 
-//Philips w732 partition_define.h
+//Lenovo A690 NAND partition_define.h
 
 #define KB  (1024)
 #define MB  (1024 * KB)
 #define GB  (1024 * MB)
 
 #define PART_SIZE_PRELOADER			(256*KB)
-#define PART_SIZE_DSP_BL			(5888*KB) 
-#define PART_SIZE_MBR			(16*KB)
-#define PART_SIZE_EBR1			(368*KB)
-#define PART_SIZE_PMT			(4096*KB)
+#define PART_SIZE_DSP_BL			(768*KB)
 #define PART_SIZE_NVRAM			(3072*KB)
 #define PART_SIZE_SECCFG			(128*KB)
 #define PART_SIZE_UBOOT			(384*KB)
-#define PART_SIZE_BOOTIMG			(6144*KB)
-#define PART_SIZE_RECOVERY			(6144*KB)
-#define PART_SIZE_SEC_RO			(6144*KB)
+#define PART_SIZE_BOOTIMG			(5120*KB)
+#define PART_SIZE_RECOVERY			(5120*KB)
+#define PART_SIZE_SEC_RO			(1152*KB)
 #define PART_SIZE_MISC			(384*KB)
 #define PART_SIZE_LOGO			(3072*KB)
 #define PART_SIZE_EXPDB			(640*KB)
-#define PART_SIZE_EBR2			(16*KB)
-#define PART_SIZE_ANDROID			(525312*KB)
-#define PART_SIZE_CACHE			(525312*KB)
-#define PART_SIZE_USRDATA			(1073152*KB)
-#define PART_SIZE_FAT			(0*KB)
+#define PART_SIZE_FAT			(5120*KB)
+#define PART_SIZE_ANDROID			(302080*KB)
+#define PART_SIZE_CACHE			(61440*KB)
+#define PART_SIZE_USRDATA			(0*KB)
 #define PART_SIZE_BMTPOOL			(0x50)
 
-
-#define PART_NUM			20
-
-#define MBR_START_ADDRESS_BYTE			(6144*KB)
-
 #define WRITE_SIZE_Byte		512
+
+#define PART_NUM			15
+
+#define MBR_START_ADDRESS_BYTE			(*1024)
+
 typedef enum  {
 	EMMC = 1,
 	NAND = 2,
@@ -51,7 +46,6 @@ typedef enum {
 	GP_3,
 	GP_4,
 } Region;
-
 
 struct excel_info{
 	char * name;
