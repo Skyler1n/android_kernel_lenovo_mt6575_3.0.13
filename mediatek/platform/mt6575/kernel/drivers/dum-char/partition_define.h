f1c5813018afa96b840563e31bf1eb413e2d5368
Skyler1n <Skyler1n@outlook.com>
2026-07-13 18:24:15 +0800

    fix(dumchar): use w732 partition define to fix NULL dereference
    Signed-off-by: Skyler1n <Skyler1n@outlook.com>

--- mediatek/platform/mt6575/kernel/drivers/dum-char/partition_define.h
+++ mediatek/platform/mt6575/kernel/drivers/dum-char/partition_define.h
@@ -3,14 +3,14 @@
 #define __PARTITION_DEFINE_H__
 
 
-
+//Philips w732 partition_define.h
 
 #define KB  (1024)
 #define MB  (1024 * KB)
 #define GB  (1024 * MB)
 
 #define PART_SIZE_PRELOADER			(256*KB)
-#define PART_SIZE_DSP_BL			(1920*KB)
+#define PART_SIZE_DSP_BL			(5888*KB) 
 #define PART_SIZE_MBR			(16*KB)
 #define PART_SIZE_EBR1			(368*KB)
 #define PART_SIZE_PMT			(4096*KB)
@@ -23,21 +23,17 @@
 #define PART_SIZE_MISC			(384*KB)
 #define PART_SIZE_LOGO			(3072*KB)
 #define PART_SIZE_EXPDB			(640*KB)
-#define PART_SIZE_PRO_INFO			(512*KB)
 #define PART_SIZE_EBR2			(16*KB)
-#define PART_SIZE_MOBILE_INFO			(8192*KB)
-#define PART_SIZE_EBR3			(16*KB)
-#define PART_SIZE_ANDROID			(1050624*KB)
+#define PART_SIZE_ANDROID			(525312*KB)
 #define PART_SIZE_CACHE			(525312*KB)
-#define PART_SIZE_USRDATA			(1050624*KB)
+#define PART_SIZE_USRDATA			(1073152*KB)
 #define PART_SIZE_FAT			(0*KB)
-#define PART_SIZE_OTP			(44032*KB)
-#define PART_SIZE_BMTPOOL			(0xa8)
+#define PART_SIZE_BMTPOOL			(0x50)
 
 
-#define PART_NUM			24
+#define PART_NUM			20
 
-#define MBR_START_ADDRESS_BYTE			(2176*KB)
+#define MBR_START_ADDRESS_BYTE			(6144*KB)
 
 #define WRITE_SIZE_Byte		512
 typedef enum  {
